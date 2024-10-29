a42fa3057386a7f8f6c9d8d59ef2b9a34c0bfcc2
Fern <vietlinhqv@gmail.com>
2024-10-29 21:53:56 +0700

    feat: Add issuer canister

--- src/frontend/src/config.js
+++ src/frontend/src/config.js
@@ -1,10 +1,11 @@
 export const IS_DEV = import.meta.env.DEV;
-export const DEVRIVATION_ORIGIN = IS_DEV ? 'https://y3yam-6aaaa-aaaap-qb7aq-cai.icp0.io' : 'https://y3yam-6aaaa-aaaap-qb7aq-cai.icp0.io';
+export const DEVRIVATION_ORIGIN = IS_DEV ? 'http://localhost:3000' : 'https://y3yam-6aaaa-aaaap-qb7aq-cai.icp0.io';
 export const END_POINT = IS_DEV ? 'http://localhost:4943' : 'https://icp0.io';
-export const INTERNET_INDENTITY = IS_DEV ? 'https://identity.ic0.app' : 'https://identity.ic0.app';
+export const INTERNET_INDENTITY = IS_DEV ? 'http://be2us-64aaa-aaaaa-qaabq-cai.localhost:4943' : 'https://identity.ic0.app';
 export const BACKEND_CANISTER_ID = IS_DEV ? 'cbopz-duaaa-aaaaa-qaaka-cai' : '3c7yh-4aaaa-aaaap-qhria-cai';
 export const FRONTEND_CANISTER_ID = IS_DEV ? 'cgpjn-omaaa-aaaaa-qaakq-cai' : '3f66t-ryaaa-aaaap-qhriq-cai';
 export const VC_VALIDATOR_CANISTER_ID = IS_DEV ? 'cpmcr-yeaaa-aaaaa-qaala-cai' : 'uspxb-mqaaa-aaaap-qhxqq-cai';
+export const ISSUER_CANISTER_ID = IS_DEV ? 'cinef-v4aaa-aaaaa-qaalq-cai' : 'znqos-ziaaa-aaaap-qkmia-cai';
 console.log('IS_DEV', IS_DEV, END_POINT);
 
 export const FRONTEND_DOMAIN = `https://blockid.cc`;
